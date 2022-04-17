57fe91ad56b52b1f87f17df99b33e7611158ac61
Vipan Kumar <vipan.webdeveloper@gmail.com>
2022-04-17 12:52:37 +0530

    base

--- database/migrations/2022_04_17_065707_create_scores_table.php
+++ database/migrations/2022_04_17_065707_create_scores_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('scores', function (Blueprint $table) {
             $table->id();
+            $table->bigInteger('user_id')->unsigned();
+            $table->bigInteger('score')->default(0);
             $table->timestamps();
         });
     }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,8 +18,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
-
+Route::group(['middleware'=>'auth','prefix'=>'admin'], function(){
+    Route::view('/dashboard', 'dashboard')->name('dashboard');
+    Route::controller(TaskController::class)->group(function(){
+        Route::get('tasks','index')->name('tasks');
+    });
+});
 require __DIR__.'/auth.php';
